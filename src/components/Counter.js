c4d7503b3b669012293048f8e37efedb7cb66bfe
MatchuPitchu <michael.flohr@posteo.de>
2022-01-18 12:51:36 +0100

    multiple states

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,21 +9,25 @@ const Counter = () => {
   const dispatch = useDispatch();
   // pass in an anonymous fn that determines which piece of state you want to extract from store;
   // with useSelector, subscription is automatically set up to the store for this component;
-  // if component will be unmounted, subscription is also cleared automatically
+  // if component will be unmounted, subscription is also cleared automatically;
+  // if state changes, new state is returned automatically and leads to re-evaluation of component
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   // dispatch action types
   const incrementHandler = () => dispatch({ type: 'increment' });
+  const increaseHandler = () => dispatch({ type: 'increase', value: 5 });
   const decrementHandler = () => dispatch({ type: 'decrement' });
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => dispatch({ type: 'toggle' });
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div className={classes.btns}>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,28 @@
 import { createStore } from 'redux';
 
+const initialState = { counter: 0, showCounter: true };
+
+// with TypeScript: good approach to use general Enums for action types
+
 // reducer function
-const reducerFn = (state = { counter: 0 }, action) => {
+const reducerFn = (state = initialState, action) => {
   if (action.type === 'increment') {
-    return { counter: state.counter + 1 };
+    // Redux replaces existing state, NOT merges new state into prev state
+    return { counter: state.counter + 1, showCounter: state.showCounter };
   }
+
+  if (action.type === 'increase') {
+    return { counter: state.counter + action.value, showCounter: state.showCounter };
+  }
+
   if (action.type === 'decrement') {
-    return { counter: state.counter - 1 };
+    return { counter: state.counter - 1, showCounter: state.showCounter };
   }
+
+  if (action.type === 'toggle') {
+    return { counter: state.counter, showCounter: !state.showCounter };
+  }
+
   return state;
 };
 
